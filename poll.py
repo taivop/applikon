65383ec8bcd683ed3f4b046189738bdf6e7d0212
taivop <taivo@pungas.ee>
2021-09-15 11:32:29 +0300

    run for all sensors

--- poll.py
+++ poll.py
@@ -1,5 +1,6 @@
 import asyncio
 import json
+import logging
 import os
 
 import websockets
@@ -10,6 +11,11 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 from parse import parse_sensor_data
 
 load_dotenv()  # Take environment variables from .env. See .env.example.
+
+# Setup logging
+logging.basicConfig()
+logger = logging.getLogger("poll")
+logger.setLevel(logging.INFO)
 
 INFLUXDB_TOKEN = os.environ["INFLUXDB_TOKEN"]
 INFLUXDB_URL = os.environ["INFLUXDB_URL"]
@@ -26,15 +32,16 @@ hosts = reactors_conf["hosts"]
 
 async def poll_once(host):
     uri = f"ws://{host['address']}"
-    print(uri)
-    async with websockets.connect(uri) as websocket:
-        print(f"connected to {uri}")
+    logger.info("Connecting to '%s' at '%s'", host["name"], uri)
 
+    async with websockets.connect(uri) as websocket:
         # Ask for all sensor values
         await websocket.send("J1.15S\r")
         message = await websocket.recv()
         sensor_data = parse_sensor_data(message.decode("utf-8"))
+        logger.debug("'%s' readings: %s", host["name"], sensor_data)
 
+        # Assemble message for InfluxDB
         sequence = []
         for field in sensor_data:
             sequence.append(f"{field},host={uri} {field}={sensor_data[field]}")
