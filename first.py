661865dfe93b6417d80addb074b2bb6ed50b4cb3
taivop <taivo@pungas.ee>
2021-09-15 10:47:28 +0300

    pre-commit

--- first.py
+++ first.py
@@ -1,38 +1,39 @@
 import asyncio
+
 import websockets
-
-from parse_data import parse_sensor_data
-
-from datetime import datetime
-
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
+
+from parse_data import parse_sensor_data
 
 # You can generate a Token from the "Tokens Tab" in the UI
 token = "jqWknvpNYX1jdkBDFlFhvZfObI6oyHkQXcPow0aMoW84hOyI5vl7gsBQ1b0kXEZEvwovoDJa24baQJczYTou-A=="
 org = "sq42na@gmail.com"
 bucket = "sq42na's Bucket"
 
-client = InfluxDBClient(url="https://eu-central-1-1.aws.cloud2.influxdata.com", token=token)
+client = InfluxDBClient(
+    url="https://eu-central-1-1.aws.cloud2.influxdata.com", token=token
+)
 write_api = client.write_api(write_options=SYNCHRONOUS)
+
 
 async def listen():
     uri = "ws://192.168.1.241"
     async with websockets.connect(uri) as websocket:
         print(f"connected to {uri}")
 
-        await websocket.send("J1.15S\r") # Update all sensor values
+        await websocket.send("J1.15S\r")  # Update all sensor values
         message = await websocket.recv()
         sensor_data = parse_sensor_data(message.decode("utf-8"))
-        
+
         sequence = []
         for field in sensor_data:
             sequence.append(f"{field},host={uri} {field}={sensor_data[field]}")
-        
+
         write_api.write(bucket, org, sequence)
 
-
-        #async for message in websocket:
+        # async for message in websocket:
         #    await consumer(message)
+
 
 asyncio.run(listen())
--- parse_data.py
+++ parse_data.py
@@ -2,18 +2,10 @@ import re
 
 re_message = re.compile(r"J1\.15\.(\d+)A(.*)")
 
-sensor_map = {
-    0: "pH",
-    1: "do2",
-    2: "temperature",
-    5: "o2",
-    6: "co2",
-    13: "stirrer"
-}
+sensor_map = {0: "pH", 1: "do2", 2: "temperature", 5: "o2", 6: "co2", 13: "stirrer"}
 
-type_map = {
-    13: int
-}
+type_map = {13: int}
+
 
 def parse_sensor_data(data_string):
     messages = [m for m in data_string.split("\r") if len(m)]
@@ -27,13 +19,16 @@ def parse_sensor_data(data_string):
             datapoint_value = m_search.group(2)
 
             if datapoint_id in sensor_map:
-                #print(f"{datapoint_id:<2} {sensor_map[datapoint_id]:<12} -> {datapoint_value:>5}")
+                # print(f"{datapoint_id:<2} {sensor_map[datapoint_id]:<12} -> {datapoint_value:>5}")
                 data_type_convert = type_map.get(datapoint_id, float)
-                sensor_data[sensor_map[datapoint_id]] = data_type_convert(datapoint_value)
+                sensor_data[sensor_map[datapoint_id]] = data_type_convert(
+                    datapoint_value
+                )
         else:
             print("NO MESSAGE FOUND")
-    
+
     return sensor_data
+
 
 if __name__ == "__main__":
     with open("data_example.txt") as f:
